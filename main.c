6f0e522f1ac359dfb52f9595a57f046cf2f9379f
Igor <86932510+igor-carlos@users.noreply.github.com>
2022-06-29 00:56:29 -0300

    v2

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <Servo.h>
-
 #include <ESP8266WiFi.h>
-
 #include <PubSubClient.h>
-
-#define MSG_BUFFER_SIZE(50)
+#define MSG_BUFFER_SIZE 50
 
 IPAddress mqtt_server(192, 100, 247, 3);
 WiFiClient espClient;
+// 35.157.18.117
 
 PubSubClient client(espClient);
+
+// o roxo é no vu
+// o marrom é no 3v
 
 const int VD1 = 5;
 const int VD2 = 4;
@@ -18,27 +19,29 @@ const int VD4 = 2;
 const int VD5 = 14;
 
 int SODistCm = 0;
-const int SOTrigPin = VD1;
-const int SOEchoPin = VD2;
+const int SOTrigPin = VD3;
+const int SOEchoPin = VD4;
 
 int STDistCm = 0;
-const int STTrigPin = VD3;
-const int STEchoPin = VD4;
+const int STTrigPin = VD1;
+const int STEchoPin = VD2;
 
 Servo servo;
 int servoPosition = 0;
 const int servoPin = VD5;
 const int minimunAngle = 500;
 const int maximunAngle = 2500;
-const int angleToRotate = 90;
+const int angleToRotate = 20;
 const int delayToRotateInMs = 5;
 
 const int distToOpenCm = 10;
+const int distMaxToSentMessage = 20;
+const int distMinToSentMessage = 10;
 const int delayToReturnCloseInMs = 5000;
 
 const int mqttPort = 11883;
-const char * ssid = "AULA_IOT";
-const char * password = "unismg123!";
+const char * ssid = "Tone 2.4 GHz";
+const char * password = "tone1970";
 char msg[MSG_BUFFER_SIZE];
 const char * topic = topic;
 
@@ -77,15 +80,15 @@ void callback(char * topic, byte * payload, unsigned int length) {
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Tentativa MQTT para conexão...");
-    if (client.connect("grupo04", "alunos", "$3nh@321!")) {
+    if (client.connect("b547a30b-5110-4f2a-8d01-ec137257ffc6","mqttserverunisbilly","billy$#@!4321")) {
       Serial.println(" Conectado");
       // client.publish("grupo04", "teste");
       // client.subscribe(topic);
     } else {
       Serial.print(" Falha na Conexão, rc=");
       Serial.print(client.state());
-      Serial.println(" tente novamente em 3 segundos");
-      delay(3000);
+      Serial.println(" tente novamente em 1 segundos");
+      delay(1000);
     }
   }
 }
@@ -121,15 +124,17 @@ void setup() {
   servo.attach(servoPin, minimunAngle, maximunAngle);
 
   setup_wifi();
-  client.setServer(mqtt_server, mqttPort);
+  const char *xmqtt_broker = "broker.hivemq.com";
+  // client.setServer(xmqtt_broker, mqttPort);
+  client.setServer(xmqtt_broker, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
-  // if (!client.connected()) {
-  //   reconnect();
-  // }
-  // client.loop();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
   SODistCm = 0.01723 * readUltrasonicDistance(SOTrigPin, SOEchoPin);
   Serial.print("sensor one distance in cm: ");
@@ -141,14 +146,18 @@ void loop() {
 
   if (SODistCm <= distToOpenCm) {
     Serial.println("servo triggered !");
-    rotateClockwise();
-    delay(delayToReturnCloseInMs);
     rotateCounterClockwise();
+    delay(delayToReturnCloseInMs);
+    rotateClockwise();
   }
 
-  if (STDistCm <= 15) {
-    Serial.println("enviar mensagem pro MQTT");
-    // client.publish(topic, "lixeira cheia");
+  if (STDistCm <= distMinToSentMessage || STDistCm >= distMaxToSentMessage) {
+    // client.subscribe("grupo04/estadoLixeira")
+    client.publish("grupo04/estadoLixeira", "cheia");
+    Serial.println("Enviou que está cheia");
+  } else {
+    client.publish("grupo04/estadoLixeira", "vazia");
+    Serial.println("Enviou que está vazia");
   }
 
   delay(500);
